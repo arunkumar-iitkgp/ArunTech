c5ac1ab1150290f047a1d333f0bb65a9311a4379
arunkumar <arunkumar.m20.iitkgp@gmail.com>
2026-05-02 15:07:02 -0400

    commit 1.1 Practice task

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -21,9 +21,30 @@ function show(){
     var functionVar = "I'm a block-scoped var";
     let functionLet = "I'm a block-scoped let";
     const functionConst = "I'm a block-scoped const";
+    
+        // Declaring variables
+        var blockVar = "I am a block var";
+        let blockLet = "I am a block let";
+        const blockConst = "I am a block const";
+    
+        // Reassigning inside the block
+        blockVar = "Reassigned block var";
+        blockLet = "Reassigned block let";
+        // blockConst = "New Const"; // UNCOMMENT THIS TO SEE ERROR: Assignment to constant variable.
+    
+        console.log(blockVar); // Works
+        console.log(blockLet); // Works
+    
     }
     show();
     
     console.log(functionVar); // Throws ReferenceError
     console.log(functionLet); // Throws ReferenceError
     console.log(functionConst); // Throws ReferenceError
+    
+    // Reassigning outside the block
+blockVar = "Changed again outside"; // Works! (var is not block-scoped)
+console.log(blockVar);
+
+// blockLet = "Changed outside"; // ERROR: blockLet is not defined
+// blockConst = "Changed outside"; // ERROR: blockConst is not defined
