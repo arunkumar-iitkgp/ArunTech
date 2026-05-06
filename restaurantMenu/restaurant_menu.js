a4122e909a18c13da19c89fb98dd78ad8cc07647
arunkumar <arunkumar.m20.iitkgp@gmail.com>
2026-05-06 15:32:28 -0400

    commit 6.1 : Restaurant Menu Iteration - Practice task

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $20', 'Pasta - $20', 'Burger - $20', 'Salmon - $20'];
+const dessertMenu = ['Cake - $20', 'Ice Cream - $20', 'Pudding - $20', 'Fruit Salad - $20'];
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
  let mainCourseItem = '';
