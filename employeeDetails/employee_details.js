ef808e6e63589e0cd4e0ee2c612d958b56c6bd2d
arunkumar <arunkumar.m20.iitkgp@gmail.com>
2026-05-07 11:32:56 -0400

    commit 7.1 : Practice task

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,13 +1,13 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript' },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java' },
       //... More employee records can be added here
     ];
 // Function to display all employees
 function displayEmployees() {
     const totalEmployees = employees
-        .map(employee => `<p>${employee.id}: ${employee.name} - ${employee.department} - ${employee.salary}</p>`)
+        .map(employee => `<p>${employee.id}: ${employee.name} - ${employee.department} - ${employee.salary} - ${employee.specialization}</p>`)
         .join('');
     document.getElementById('employeesDetails').innerHTML = totalEmployees;
 }
@@ -29,3 +29,15 @@ function findEmployeeById(employeeId) {
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
        }
    }
+   function findspecialization(spec) {
+    // .find() looks for the first match
+    const foundEmployee = employees.find(employee => employee.specialization === spec);
+
+    if (foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML = 
+            `<p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} - ${foundEmployee.specialization}</p>`;
+    } else {
+        document.getElementById('employeesDetails').innerHTML = 
+            'No employee has been found with this specialization';
+    }
+}
