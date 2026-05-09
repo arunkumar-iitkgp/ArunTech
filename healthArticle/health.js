5ef8d8c817772cf6c1a0502530d489d7c9df7dde
arunkumar <arunkumar.m20.iitkgp@gmail.com>
2026-05-09 14:44:55 -0400

    commit 11.1 : practice task - Health Articles

---
+++ healthArticle/health.js
@@ -0,0 +1,27 @@
+var xhr = new XMLHttpRequest();
+var url = './health.json';
+
+xhr.open('GET', url, true);
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+    var articles = xhr.response.articles;
+    var articlesDiv = document.getElementById('articles');
+
+    articles.forEach(function(article) {
+        var articleDiv = document.createElement('div');
+        articleDiv.classList.add('article');
+
+        var title = document.createElement('h2');
+        title.textContent = article.title;
+
+        var description = document.createElement('p');
+        description.textContent = article.description;
+
+        articleDiv.appendChild(title);
+        articleDiv.appendChild(description);
+        articlesDiv.appendChild(articleDiv);
+    });
+};
+
+xhr.send();
